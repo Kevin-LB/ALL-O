f5d6d97656ac9475781f1730487f6b3c0b0d68c4
Jean-MarcJ <jean-marc.jorite@etu.univ-orleans.fr>
2024-04-07 14:21:02 +0200

    factorisation code

--- lib/UI/pageAdd.dart
+++ lib/UI/pageAdd.dart
@@ -5,7 +5,6 @@ import 'package:allo/UI/acceuil/home.dart';
 import 'package:allo/db/alloDB.dart';
 import 'package:allo/db/supabase.dart';
 import 'package:allo/models/appartenirAnnonce.dart';
-import 'package:allo/models/categorie.dart';
 import 'package:allo/models/annonce.dart';
 
 import 'package:flutter/material.dart';
@@ -70,190 +69,180 @@ class _PageAddState extends State<PageAdd> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF3C3838),
-          title: FittedBox(
-            fit: BoxFit.scaleDown,
-            child: RichText(
-              text: const TextSpan(
-                text: "A",
-                style: TextStyle(
-                  color: Color(0xff57A85A),
-                  fontSize: 45.0,
-                ),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "'llo",
-                    style: TextStyle(
-                      color: Color(0xffFFFFFF),
-                      fontSize: 30.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        appBar: buildAppBar(),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              const Padding(
-                  padding: EdgeInsets.only(top: 30, bottom: 70.0),
-                  child: Text("Créer une Annonce",
-                      style:
-                          TextStyle(fontSize: 30.0, color: Color(0xffFFFFFF)))),
-              Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: Center(
-                    child: Container(
-                      width: 300,
-                      color: const Color(0xFF8C8585),
-                      child: TextFormField(
-                        controller: _annonceController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Titre de l'annonce",
-                          labelStyle: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromRGBO(255, 255, 255, 0.5)),
-                        ),
-                      ),
-                    ),
-                  )),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: Center(
-                  child: Container(
-                    width: 300,
-                    height: 150,
-                    color: const Color(0xFF8C8585),
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: _descriptionController,
-                      maxLines: null,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Veuillez entrer une description';
-                        }
-                        return null;
-                      },
-                      decoration: const InputDecoration(
-                        labelText: 'Description',
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Center(
-                    child: Container(
-                      width: 300,
-                      color: const Color(0xFF8C8585),
-                      child: TextFormField(
-                        readOnly: true,
-                        onTap: () {
-                          dialogBuilder(context, onCheckboxChanged);
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Catégorie',
-                          hintText: selectedCategories.isEmpty
-                              ? 'Sélectionnez une catégorie'
-                              : selectedCategories,
-                          border: const OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                  )),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: ButtonSelect(
-                  text: "Valider",
-                  onPressed: () async {
-                    final List<Annonce> annonceList = await allDb.annonces();
-                    if (annonceList.isEmpty) {
-                      String libelleAnnonce = _annonceController.text;
-                      String descriptionAnnonce = _descriptionController.text;
-                      print("libelleAnnonce: $libelleAnnonce");
-                      print("descriptionAnnonce: $descriptionAnnonce");
-                      final annonceInsert = Annonce(
-                        id: 1,
-                        libelle: libelleAnnonce,
-                        description: descriptionAnnonce,
-                        datePost: DateTime.now(),
-                        idB: 1,
-                        idU: 1,
-                      );
-                      print(
-                          "les catégories sélectionnées: $selectedCategories");
-                      if (selectedCategories.isNotEmpty) {
-                        print(
-                            "les catégories sélectionnées: $selectedCategories");
-                        if (selectedCategories.isNotEmpty) {
-                          String categories = selectedCategories.isNotEmpty
-                              ? selectedCategories
-                              : '';
-                          for (final categorie in categories.split(', ')) {
-                            int idCategorie =
-                                await allDb.getCategorieId(categorie);
-                            print("Test insertAppartenirAnnonce");
-                            await allDb
-                                .insertAppartenirAnnonce(Appartenir_Annonce(
-                              idA: annonceInsert.id,
-                              idC: idCategorie,
-                            ));
-                          }
-                        }
-                      }
+              buildTitle(),
+              buildAnnonceField(),
+              buildDescriptionField(),
+              buildCategoryField(),
+              buildValidationButton(),
+            ],
+          ),
+        ));
+  }
 
-                      await allDb.insertAnnonce(annonceInsert);
-                      if (mounted) {
-                        navigateToHome(context);
-                      }
-                    } else {
-                      final id_annonce = annonceList.last.id + 1;
-                      String libelleAnnonce = _annonceController.text;
-                      String descriptionAnnonce = _descriptionController.text;
-                      final annonceInsert = Annonce(
-                        id: id_annonce,
-                        libelle: libelleAnnonce,
-                        description: descriptionAnnonce,
-                        datePost: DateTime.now(),
-                        idB: 1,
-                        idU: 1,
-                      );
-                      await allDb.insertAnnonce(annonceInsert);
-                      print(
-                          "les catégories sélectionnées: $selectedCategories");
-                      if (selectedCategories.isNotEmpty) {
-                        String categories = selectedCategories.isNotEmpty
-                            ? selectedCategories
-                            : '';
-                        for (final categorie in categories.split(', ')) {
-                          int idCategorie =
-                              await allDb.getCategorieId(categorie);
-                          print("Test insertAppartenirAnnonce");
-                          await allDb
-                              .insertAppartenirAnnonce(Appartenir_Annonce(
-                            idA: annonceInsert.id,
-                            idC: idCategorie,
-                          ));
-                        }
-                      }
-                      if (mounted) {
-                        navigateToHome(context);
-                      }
-                    }
-                  },
-                  buttonColor: const Color(0xFFD9D9D9),
-                  textColor: const Color(0xff201D1D),
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: const Color(0xFF3C3838),
+      title: FittedBox(
+        fit: BoxFit.scaleDown,
+        child: RichText(
+          text: const TextSpan(
+            text: "A",
+            style: TextStyle(
+              color: Color(0xff57A85A),
+              fontSize: 45.0,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: "'llo",
+                style: TextStyle(
+                  color: Color(0xffFFFFFF),
+                  fontSize: 30.0,
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Padding buildTitle() {
+    return const Padding(
+        padding: EdgeInsets.only(top: 30, bottom: 70.0),
+        child: Text("Créer une Annonce",
+            style: TextStyle(fontSize: 30.0, color: Color(0xffFFFFFF))));
+  }
+
+  Padding buildAnnonceField() {
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Center(
+          child: Container(
+            width: 300,
+            color: const Color(0xFF8C8585),
+            child: TextFormField(
+              controller: _annonceController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Titre de l'annonce",
+                labelStyle: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(255, 255, 255, 0.5)),
+              ),
+            ),
+          ),
         ));
+  }
+
+  Padding buildDescriptionField() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Center(
+        child: Container(
+          width: 300,
+          height: 150,
+          color: const Color(0xFF8C8585),
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _descriptionController,
+            maxLines: null,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Veuillez entrer une description';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: 'Description',
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding buildCategoryField() {
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Center(
+          child: Container(
+            width: 300,
+            color: const Color(0xFF8C8585),
+            child: TextFormField(
+              readOnly: true,
+              onTap: () {
+                dialogBuilder(context, onCheckboxChanged);
+              },
+              decoration: InputDecoration(
+                labelText: 'Catégorie',
+                hintText: selectedCategories.isEmpty
+                    ? 'Sélectionnez une catégorie'
+                    : selectedCategories,
+                border: const OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Padding buildValidationButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: ButtonSelect(
+        text: "Valider",
+        onPressed: () async {
+          await validateAndSubmit();
+        },
+        buttonColor: const Color(0xFFD9D9D9),
+        textColor: const Color(0xff201D1D),
+      ),
+    );
+  }
+
+  Future<void> validateAndSubmit() async {
+    final List<Annonce> annonceList = await allDb.annonces();
+    if (annonceList.isEmpty) {
+      await insertAnnonce(1);
+    } else {
+      final id_annonce = annonceList.last.id + 1;
+      await insertAnnonce(id_annonce);
+    }
+  }
+
+  Future<void> insertAnnonce(int id_annonce) async {
+    String libelleAnnonce = _annonceController.text;
+    String descriptionAnnonce = _descriptionController.text;
+    final annonceInsert = Annonce(
+      id: id_annonce,
+      libelle: libelleAnnonce,
+      description: descriptionAnnonce,
+      datePost: DateTime.now(),
+      idB: 1,
+      idU: 1,
+    );
+    await allDb.insertAnnonce(annonceInsert);
+    print("les catégories sélectionnées: $selectedCategories");
+    if (selectedCategories.isNotEmpty) {
+      String categories =
+          selectedCategories.isNotEmpty ? selectedCategories : '';
+      for (final categorie in categories.split(', ')) {
+        int idCategorie = await allDb.getCategorieId(categorie);
+        print("Test insertAppartenirAnnonce");
+        await allDb.insertAppartenirAnnonce(Appartenir_Annonce(
+          idA: annonceInsert.id,
+          idC: idCategorie,
+        ));
+      }
+    }
+    if (mounted) {
+      navigateToHome(context);
+    }
   }
 
   Future<void> dialogBuilder(
